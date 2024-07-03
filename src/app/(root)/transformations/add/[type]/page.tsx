26b4c19aad8bb3ea82fc120a2c07bd3df109f4bf
unknown <harshjagtap2003@gmail.com>
2024-07-03 11:48:30 +0530

    chore: form-template

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Header from '@/components/shared/Header';
+import { SearchParamProps } from '../../../../../../types';
+import { transformationTypes } from '../../../../../../constants';
+import TransformationForm from '@/components/shared/TransformationForm';
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = ({ params: {type} }: SearchParamProps) => {
+  const transformation = transformationTypes[type]
   return (
-    <div>AddTransformationTypePage</div>
+    <>
+      <Header 
+        title={transformation.title} 
+        subtitle={transformation.subTitle}  
+      />
+      <TransformationForm />
+    </>
   )
 }
 
