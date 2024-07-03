ede2921d1e22639c0f0be576678b7601f365d701
unknown <harshjagtap2003@gmail.com>
2024-07-03 12:51:35 +0530

    chore: form-initial-setup/middleware added

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import Header from '@/components/shared/Header';
-import { SearchParamProps } from '../../../../../../types';
+import { SearchParamProps, TransformationTypeKey } from '../../../../../../types';
 import { transformationTypes } from '../../../../../../constants';
 import TransformationForm from '@/components/shared/TransformationForm';
+import { auth } from '@clerk/nextjs/server'
+import { getUserById } from "../../../../../lib/actions/user.action"
+import { redirect } from 'next/navigation';
 
-const AddTransformationTypePage = ({ params: {type} }: SearchParamProps) => {
-  const transformation = transformationTypes[type]
+const AddTransformationTypePage = async ({ params: {type} }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+  const { userId } = auth();
+
+  if(!userId) redirect('/sign-in');
+  const user = await getUserById(userId);
+
   return (
     <>
-      <Header 
-        title={transformation.title} 
-        subtitle={transformation.subTitle}  
+      <Header
+        title={transformation.title}
+        subtitle={transformation.subTitle}
       />
-      <TransformationForm />
+      <TransformationForm 
+        action='Add' 
+        userId={user._id} 
+        type={transformation.type as TransformationTypeKey} 
+        creditBalance={user.creditBalance} 
+      />
     </>
   )
 }
--- src/components/shared/TransformationForm.tsx
+++ src/components/shared/TransformationForm.tsx
@@ -1,8 +1,74 @@
-import React from 'react'
+"use client"
+ 
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { defaultValues } from "../../../constants"
+import { TransformationFormProps } from "../../../types"
 
-const TransformationForm = () => {
+
+const formSchema = z.object({
+  // username: z.string().min(2).max(50),
+  title: z.string(),
+  aspectRatio: z.string().optional(),
+  color: z.string().optional(),
+  prompt: z.string().optional(),
+  publicId: z.string()
+})
+
+const TransformationForm = ({action, data = null}: TransformationFormProps) => {
+
+  const initialValues = data && action === "Update" ? {
+    title: data?.title,
+    aspectRatio: data?.aspectRatio,
+    color: data?.color,
+    prompt: data?.prompt,
+    publicId: data?.publicId,
+  } : defaultValues
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialValues,
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
   return (
-    <div>TransformationForm</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
 
